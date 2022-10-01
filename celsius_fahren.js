9e909104643014f72ed5777a95183b6e733d880a
SheikMohideen007 <69137155+SheikMohideen007@users.noreply.github.com>
2022-10-01 23:59:10 +0530

    Add files via upload

---
+++ celsius_fahren.js
@@ -0,0 +1,11 @@
+//for celsius to fahrenheit
+var num = parseInt(60);
+var fahrenheit = (num * 9) / 5 + 32;
+console.log(
+  `${num} degree celsius is equal to ${fahrenheit} degree fahrenheit`
+);
+
+//for fahrenheit to celsius
+var num = parseInt(45);
+var celsius = ((num - 32) * 5) / 9;
+console.log(`${num} degree fahrenheit is equal to ${celsius} degree celsius`);
