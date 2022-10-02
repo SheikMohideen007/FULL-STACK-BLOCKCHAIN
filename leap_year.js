0e34d1a9256631efd121d34d382f155a94c1ffdb
SheikMohideen007 <69137155+SheikMohideen007@users.noreply.github.com>
2022-10-02 17:27:56 +0530

    Delete leap_year.js

--- leap_year.js
+++
@@ -1,8 +0,0 @@
-var num = 2022;
-var year = parseInt(num);
-
-if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0)) {
-  console.log(`${year} is Leap year`);
-} else {
-  console.log(`${year} is not a Leap year`);
-}
